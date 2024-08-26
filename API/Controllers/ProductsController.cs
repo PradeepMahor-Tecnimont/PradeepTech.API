fb3fab31cacb629f0917abdc1047afba16c88fc3
Mahor Pradeepkumar (Mumbai - IT) <p.mahor@tecnimont.in>
2024-08-26 11:07:21 +0530

    Specification Class add for IQuery

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -13,7 +13,8 @@ namespace API.Controllers
     {
         // GET: api/Products
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand,
+                   string? type, string? sort)
         {
             return Ok(await repo.ListAllAsync());
         }
--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -10,6 +10,10 @@ namespace Core.Interfaces
 
         Task<IReadOnlyList<T>> ListAllAsync();
 
+        Task<T?> GetEntityWithSpec(ISpecification<T> spec);
+
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+
         void Add(T entity);
 
         void Update(T entity);
